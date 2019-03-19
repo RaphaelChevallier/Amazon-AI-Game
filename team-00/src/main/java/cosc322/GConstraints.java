9a6a0e74035966632a5bb85d767dfbba0178651b
tyreln <tyrelnarciso@hotmail.com>
2019-03-18 19:02:43 -0700

    GConstraints finally done please review

--- team-00/src/main/java/cosc322/GConstraints.java
+++ team-00/src/main/java/cosc322/GConstraints.java
@@ -56,13 +56,69 @@ public class GConstraints {
         Qmove = new ArrayList<>();
         updatedQmove();
     }
-
-
+    
+    public GConstraints(Queen[] foe, Queen[] friend, ArrayList<Arrow> arrow){
+        this.foe = foe;
+        this.friend = friend;
+        this.arrows = arrow;
+        updateEndTurn();
+    }
+    public Queen[] findFoe(){
+        return this.foe;
+    }
+    public Queen[] findFriend(){
+        return this.friend;
+    }
 
     public void updatedQmove() {
         Qmove.clear();
         for(Queen Q: friend) {
             Qmove.addAll(getValidMoves(Q));
+        }
+    }
+    public void updateEndTurn(){
+        clearBoard();
+        for(Queen q: friend){
+            if(friend != null){
+                gameboard[q.row][q.col] = q;
+                
+            }
+        }
+        for(Queen q: foe){
+            if(foe!=null){
+                gameboard[q.row][q.col]=q;
+            }
+        }
+        for(Arrow arrow: arrows){
+            if(arrow!=null){
+                gameboard[arrow.row][arrow.col] = arrow;
+            }
+        }
+    }
+    //cloning board and game states for space efficiency
+    public GConstraints transfer(){
+        Queen[] tempFriend = new Queen[4];
+        Queen[] tempFoe = new Queen[4];
+        ArrayList<Arrow> tempArrows = new ArrayList<>();
+        for(int i = 0; i < tempFoe.length; i ++){
+            tempFriend[i] = friend[i].clone();
+            tempFoe[i] = foe[i].clone().clone();
+        }
+        if(arrows != null){
+            for(Arrow temp: arrows){
+                if(temp != null){
+                    tempArrows.add(new Arrow(temp.row, temp.col));
+                }
+            }
+        }
+        GConstraints constraints = new GConstraints(tempFriend, tempFoe, tempArrows);
+        return constraints;
+    }
+    public void clearBoard(){
+        for(int i = 0; i < 10; i++ ){
+            for(int j = 0; j < 10; j++){
+            gameboard[i][j] = null;
+        }
         }
     }
 
@@ -171,6 +227,82 @@ public class GConstraints {
         return validMoves;
 
     } // end getValidMoves
+    public ArrayList<Arrow> arrowMoves(int row, int col){
+        ArrayList<Arrow> allowedMoves = new ArrayList<>();
+        int currentRow = row;
+        int currentColumn = col;
+        //left
+        for(int i = 1; currentColumn - i>= 0; i++){
+            if(gameboard[currentRow][currentColumn-i] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow, currentColumn-i));
+            }
+        }
+        //right
+        for(int i = 1; currentColumn + i<= 9; i++){
+            if(gameboard[currentRow][currentColumn+i] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow, currentColumn+i));
+            }
+        }
+        //up
+        for(int i = 1; currentRow - i>= 0; i++){
+            if(gameboard[currentRow-i][currentColumn] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow-i, currentColumn));
+            }
+        }
+        //down
+        for(int i = 1; currentRow + i<= 9; i++){
+            if(gameboard[currentRow+i][currentColumn] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow + i, currentColumn));
+            }
+        }
+        //left and up
+        
+        for(int i = 1; currentColumn - i>= 0 && currentRow - i >=0; i++){
+            if(gameboard[currentRow - i][currentColumn-i] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow - i, currentColumn-i));
+            }
+        }
+        //left and down
+         for(int i = 1; currentColumn - i>= 0 && currentRow + i <=9; i++){
+            if(gameboard[currentRow + i][currentColumn-i] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow + i, currentColumn-i));
+            }
+        }
+         //right and down
+           for(int i = 1; currentColumn + i<=9 && currentRow + i <=9; i++){
+            if(gameboard[currentRow + i][currentColumn + i] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow + i, currentColumn + i));
+            }
+        }
+           //right and up
+             for(int i = 1; currentColumn + i <= 9 && currentRow - i >=0 ; i++){
+            if(gameboard[currentRow - i][currentColumn + i] !=null){
+                break;
+            }else{
+                allowedMoves.add(new Arrow(currentRow - i, currentColumn+i));
+            }
+        }
+            return allowedMoves;
+        
+    }
+    public void addArrow (Arrow arrow){
+        arrows.add(arrow);
+        updateEndTurn();
+    }
 
     //potential queens that can move from enemy side
     public void enemyPotentialAction() { //canEnemyMove
@@ -215,8 +347,46 @@ public class GConstraints {
                 enemyAction = true;
                 break;
             }
+        }}
+       public boolean goalTest(){
+           if(enemyAction == false || Qmove.isEmpty()){
+               if(enemyAction == false){
+                   System.out.println("We win!");
+               }
+               else{
+                   System.out.println("We lose!");
+               }
+               return true;
+           }
+           else{
+               return false;
+           }
+       }
+     // end of enemyPotentialAction
+    public void printBoard(){
+        String board = "";
+        String line = "\nx--- --- --- --- --- --- --- --- --- ---x";
+        for(int i = 0; i < 10; i++){
+            board += line + "\n";
+            for(int j = 0; j < 10; j++){
+                board+= "| ";
+            
+            if (gameboard[i][j] == null) board += "  ";
+            else if (gameboard[i][j] instanceof Queen){
+                if (gameboard[i][j] == findFoe()[0] || gameboard[i][j] == findFoe()[1] || gameboard[i][j] == findFoe()[2]  || gameboard[i][j] == findFoe()[3]){
+                    board += "B ";
+                    
+                }
+                else board += "W ";
+                
+            }else board += "a ";
+                
+            
         }
-    } // end of enemyPotentialAction
+        board +="|";
+    }
+    board += line;
+    System.out.println(board);
 
-
+}
 }
