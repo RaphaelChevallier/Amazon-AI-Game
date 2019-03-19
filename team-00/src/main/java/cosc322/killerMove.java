9d1a0cb14155fb7eb59e657d1e3061faf5a3ce00
tyreln <tyrelnarciso@hotmail.com>
2019-03-19 14:37:49 -0700

    Killer Move

--- team-00/src/main/java/cosc322/killerMove.java
+++ team-00/src/main/java/cosc322/killerMove.java
@@ -10,6 +10,23 @@ package cosc322;
  * @author Tyrel
  */
 //The goal is to take the moves from the alpha beta prune and define killer moves within them
-public class killerMove {
+public class killerMove {//killer move heuristic
+    int numberKillerMoves;
+    int ply;
+    int killerMoves[][];
+    int move;
+    
+    public killerMove(int ply, int numberOfMoves){//ply is distance from root and numberofmoves is killer move total count
+        killerMoves = new int[ply][numberOfMoves];
+        this.ply = ply;
+        
+    }
+    //pass in a list of moves from the alpha beta prune, and determine whether each is a killer move
+    public void killerPrune(int move){//should replace with data structure for list of moves
+        for(int i = killerMoves[ply].length - 2; i >= 0; i--){
+            killerMoves[ply][i + 1] = killerMoves[ply][i];
+        }
+        killerMoves[ply][0] = move;
+    }
     
 }
